63dea5686ea1b36e524167efee3c74205d0dc54c
Ricardo Pérez <ricpelo@gmail.com>
2021-11-15 19:25:06 +0100

    Controlador Depart

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Depart;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
 Route::get('/prueba', function () {
@@ -24,3 +25,5 @@ Route::get('/prueba', function () {
 Route::get('/depart', function () {
     return view('depart.index');
 });
+
+Route::get('/depart/create', [Depart::class, 'create']);
