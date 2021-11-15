7ae82bccbc7ba7d835fa1e401b891c21c42ddb51
Ricardo Pérez <ricpelo@gmail.com>
2021-11-15 19:11:22 +0100

    Instala Tailwind CSS en local

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -2,11 +2,11 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <script src="{{ asset('js/app.js') }}"></script>
 
         <title>Laravel</title>
-
-        <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
     </head>
     <body class="antialiased">
         <div class="container mx-auto">
