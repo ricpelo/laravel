8eb6491149706df89b9391136eda35aa73253b50
Ricardo Pérez <ricpelo@gmail.com>
2022-01-13 17:31:26 +0100

    Antes del jueves 13 de enero

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function reservas()
+    {
+        return $this->hasMany(Reserva::class);
+    }
 }
